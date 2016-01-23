1b778f17991ecbe64744db490d099e9b0fd32f26
Martin Fracker <martin.frackerjr@gmail.com>
2016-01-23 06:00:12 +0000

    Add script to create folders for spring 2016

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p ~/notes
+mkdir -p ~/notes/csce
+mkdir -p ~/notes/csce/481
+mkdir -p ~/notes/csce/441
+mkdir -p ~/notes/csce/443
+mkdir -p ~/notes/geog
+mkdir -p ~/notes/geog/203
+mkdir -p ~/notes/mktg
+mkdir -p ~/notes/mktg/409
+
+git clone git@github.com:towerism/csce481 ~/notes/csce/481
+git clone git@github.com:towerism/csce441 ~/notes/csce/441
+git clone git@github.com:towerism/csce443 ~/notes/csce/443
+git clone git@github.com:towerism/geog203 ~/notes/geog/203
+git clone git@github.com:towerism/mktg409 ~/notes/mktg/409
